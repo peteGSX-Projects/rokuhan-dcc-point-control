417610210f063b25b242ee42aca8f68b5b946ec5
Peter Cole <>
2021-10-30 07:21:04 +1000

    Getting my head around NmraDcc library to control these.

--- RokuhanDCC4PointControl/RokuhanDCC4PointControl.ino
+++ RokuhanDCC4PointControl/RokuhanDCC4PointControl.ino
@@ -7,18 +7,94 @@ See the README for the full list of features and instructions.
 *************************************************************/
 
 // Include NMRA DCC library.
-#include "nmradcc"
+#include <NmraDcc.h>
 
+// Print every turnout call-back message
+#define NOTIFY_TURNOUT_MSG
+
+// Print debug messages
+#define DEBUG_MSG
+
+// If below is uncommented, it will force CVs to be written to the factory defaults
+#define FORCE_RESET_FACTORY_DEFAULT_CV
+
+// Enable DCC ACK for programming and CV reading via port A1 (D15)
+#define ENABLE_DCC_ACK 15
+
+// Define the DCC input pin
+#define DCC_PIN 2
+
+// Define the number of turnouts, we need 4
+#define NUM_TURNOUTS 4              // Set Number of Turnouts
+
+// DCC decoder version
+#define DCC_DECODER_VERSION_NUM 11
+
+// Define the CV pair struct
+struct CVPair
+{
+  uint16_t  CV;
+  uint8_t   Value;
+};
+
+// To set the Turnout Addresses for this board you need to change the CV values for CV1 (CV_ACCESSORY_DECODER_ADDRESS_LSB) and 
+// CV9 (CV_ACCESSORY_DECODER_ADDRESS_MSB) in the FactoryDefaultCVs structure below. The Turnout Addresses are defined as: 
+// Base Turnout Address is: ((((CV9 * 64) + CV1) - 1) * 4) + 1 
+// With NUM_TURNOUTS 8 (above) a CV1 = 1 and CV9 = 0, the Turnout Addresses will be 1..8, for CV1 = 2 the Turnout Address is 5..12
+
+CVPair FactoryDefaultCVs [] =
+{
+  {CV_ACCESSORY_DECODER_ADDRESS_LSB, DEFAULT_ACCESSORY_DECODER_ADDRESS & 0xFF},
+  {CV_ACCESSORY_DECODER_ADDRESS_MSB, DEFAULT_ACCESSORY_DECODER_ADDRESS >> 8},
+};
+
+// Define factory default CV index
+uint8_t FactoryDefaultCVIndex = 0;
+
+// Define our NMRA DCC objects and variables
+NmraDcc  Dcc ;
+DCC_MSG  Packet ;
+uint16_t BaseTurnoutAddress;
+
+// This function is called whenever a normal DCC Turnout Packet is received
+void notifyDccAccTurnoutOutput( uint16_t Addr, uint8_t Direction, uint8_t OutputPower )
+{
+#ifdef  NOTIFY_TURNOUT_MSG
+  Serial.print("notifyDccAccTurnoutOutput: Turnout: ") ;
+  Serial.print(Addr,DEC) ;
+  Serial.print(" Direction: ");
+  Serial.print(Direction ? "Closed" : "Thrown") ;
+  Serial.print(" Output: ");
+  Serial.print(OutputPower ? "On" : "Off") ;
+#endif
+  if(( Addr >= BaseTurnoutAddress ) && ( Addr < (BaseTurnoutAddress + NUM_TURNOUTS )) && OutputPower )
+  {
+    uint16_t pinIndex = ( (Addr - BaseTurnoutAddress) << 1 ) + Direction ;
+    // This line below may be where I need to call the switching function
+    // pinPulser.addPin(outputs[pinIndex]);
+#ifdef  NOTIFY_TURNOUT_MSG
+    Serial.print(" Pin Index: ");
+    Serial.print(pinIndex,DEC);
+    // Commenting out these as they refer to the pulser pins
+    //Serial.print(" Pin: ");
+    //Serial.print(outputs[pinIndex],DEC);
+#endif
+  }
+#ifdef  NOTIFY_TURNOUT_MSG
+  Serial.println();
+#endif
+}
+
+/* This commented section is the initial 4 point code
 // define the structure for the definition of a point setup
 typedef struct {
   byte directionPin1;                   // pin to connect to input 1/3 on the L293D
   byte directionPin2;                   // pin to connect to input 2/4 on the L293D
   byte speedPin;                        // pin to connect to enable 1/2 on the L293D
   byte switchPin;                       // pin to connect the direction switch button to
-  byte currentDirection;                // current direction of the point (LOW = through, HIGH = branch)
-  byte newDirection;                    // new direction of the point (LOW = through, HIGH = branch)
+  byte currentDirection;                // current direction of the point (LOW = closed, HIGH = thrown)
+  byte newDirection;                    // new direction of the point (LOW = closed, HIGH = thrown)
   unsigned long lastSwitchMillis;       // variable for the last time it was switched
-  Switch *switchButton;                 // reference to switch object to poll the switch button
 } point_def;
 
 point_def points[4];                    // set up an array to hold the point definitions above
@@ -26,17 +102,15 @@ point_def points[4];                    // set up an array to hold the point def
 // define variables in use
 unsigned long currentMillis = 0;        // variable to hold the current Millis time
 long switchDelay = 25;                  // delay time in ms that the Rokuhan switch motor needs to be on to switch
+*/
 
-void setup() {
-  // Set up our serial output
-  Serial.begin(9600);
-  Serial.println("Rokuhan 4 Point Controller");
-
+void initTurnoutPins() {
+  /* This commented out section is the initial 4 point code
   // define our 4 points in use
-  points[0] = (point_def) {A4,A5,5,A0,LOW,LOW,0,new Switch(A0)};
-  points[1] = (point_def) {4,7,6,A1,LOW,LOW,0,new Switch(A1)};
-  points[2] = (point_def) {8,11,9,A2,LOW,LOW,0,new Switch(A2)};
-  points[3] = (point_def) {12,13,10,A3,LOW,LOW,0,new Switch(A3)};
+  points[0] = (point_def) {A4,A5,5,A0,LOW,LOW,0};
+  points[1] = (point_def) {4,7,6,A1,LOW,LOW,0};
+  points[2] = (point_def) {8,11,9,A2,LOW,LOW,0};
+  points[3] = (point_def) {12,13,10,A3,LOW,LOW,0};
 
   // set the correct pin mode and initial values for all defined pins (Switch object sets this for the button pins)
   for (int point = 0; point < 4; point++) {
@@ -48,9 +122,11 @@ void setup() {
     digitalWrite(points[point].directionPin2, LOW);
     analogWrite(points[point].speedPin, 0);
   }
+  */
 }
 
-void loop() {
+void switchTurnout() {
+  /* This is the original loop from the 4 point controller code
   for (int point = 0; point < 4; point++) {
     currentMillis = millis();                                                 // record the current millis time
     if (points[point].currentDirection != points[point].newDirection) {       // if our points aren't set to the new direction yet, need to process
@@ -80,4 +156,107 @@ void loop() {
       points[point].lastSwitchMillis = currentMillis;                         // record the time we started switching
     }
   }
+  */
 }
+
+void setup() {
+  // Set up our serial output
+  Serial.begin(115200);
+  Serial.println("Rokuhan 4 Point Controller");
+
+  // Setup which External Interrupt, the Pin it's associated with that we're using and enable the Pull-Up
+  // Many Arduino Cores now support the digitalPinToInterrupt() function that makes it easier to figure out the
+  // Interrupt Number for the Arduino Pin number, which reduces confusion. 
+#ifdef digitalPinToInterrupt
+  Dcc.pin(DCC_PIN, 0);
+#else
+  Dcc.pin(0, DCC_PIN, 1);
+#endif
+  
+  // Call the main DCC Init function to enable the DCC Receiver
+  Dcc.init( MAN_ID_DIY, DCC_DECODER_VERSION_NUM, CV29_ACCESSORY_DECODER, 0 );
+
+#ifdef DEBUG_MSG
+  Serial.print("\nNMRA DCC 8-Turnout Accessory Decoder. Ver: "); Serial.println(DCC_DECODER_VERSION_NUM,DEC);
+#endif  
+
+#ifdef FORCE_RESET_FACTORY_DEFAULT_CV
+  Serial.println("Resetting CVs to Factory Defaults");
+  notifyCVResetFactoryDefault(); 
+#endif
+
+  if( FactoryDefaultCVIndex == 0)  // Not forcing a reset CV Reset to Factory Defaults so initTurnoutPins
+    initTurnoutPins();
+}
+
+void loop() {
+  // You MUST call the NmraDcc.process() method frequently from the Arduino loop() function for correct library operation
+  Dcc.process();
+  
+  //pinPulser.process();
+  
+  if( FactoryDefaultCVIndex && Dcc.isSetCVReady())
+  {
+    FactoryDefaultCVIndex--; // Decrement first as initially it is the size of the array
+    uint16_t cv = FactoryDefaultCVs[FactoryDefaultCVIndex].CV;
+    uint8_t val = FactoryDefaultCVs[FactoryDefaultCVIndex].Value;
+#ifdef DEBUG_MSG
+    Serial.print("loop: Write Default CV: "); Serial.print(cv,DEC); Serial.print(" Value: "); Serial.println(val,DEC);
+#endif     
+    Dcc.setCV( cv, val );
+  }
+}
+
+void notifyCVChange(uint16_t CV, uint8_t Value)
+{
+#ifdef DEBUG_MSG
+  Serial.print("notifyCVChange: CV: ") ;
+  Serial.print(CV,DEC) ;
+  Serial.print(" Value: ") ;
+  Serial.println(Value, DEC) ;
+#endif  
+
+  Value = Value;  // Silence Compiler Warnings...
+
+//  if((CV == CV_ACCESSORY_DECODER_ADDRESS_MSB) || (CV == CV_ACCESSORY_DECODER_ADDRESS_LSB) ||
+//     (CV == CV_ACCESSORY_DECODER_OUTPUT_PULSE_TIME) || (CV == CV_ACCESSORY_DECODER_CDU_RECHARGE_TIME) || (CV == CV_ACCESSORY_DECODER_ACTIVE_STATE))
+//    initPinPulser();  // Some CV we care about changed so re-init the PinPulser with the new CV settings
+}
+
+void notifyCVResetFactoryDefault()
+{
+  // Make FactoryDefaultCVIndex non-zero and equal to num CV's to be reset 
+  // to flag to the loop() function that a reset to Factory Defaults needs to be done
+  FactoryDefaultCVIndex = sizeof(FactoryDefaultCVs)/sizeof(CVPair);
+};
+
+// This function is called by the NmraDcc library when a DCC ACK needs to be sent
+// Calling this function should cause an increased 60ma current drain on the power supply for 6ms to ACK a CV Read 
+#ifdef  ENABLE_DCC_ACK
+void notifyCVAck(void)
+{
+#ifdef DEBUG_MSG
+  Serial.println("notifyCVAck") ;
+#endif
+  // Configure the DCC CV Programing ACK pin for an output
+  pinMode( ENABLE_DCC_ACK, OUTPUT );
+
+  // Generate the DCC ACK 60mA pulse
+  digitalWrite( ENABLE_DCC_ACK, HIGH );
+  delay( 10 );  // The DCC Spec says 6ms but 10 makes sure... ;)
+  digitalWrite( ENABLE_DCC_ACK, LOW );
+}
+#endif
+
+#ifdef  NOTIFY_DCC_MSG
+void notifyDccMsg( DCC_MSG * Msg)
+{
+  Serial.print("notifyDccMsg: ") ;
+  for(uint8_t i = 0; i < Msg->Size; i++)
+  {
+    Serial.print(Msg->Data[i], HEX);
+    Serial.write(' ');
+  }
+  Serial.println();
+}
+#endif
